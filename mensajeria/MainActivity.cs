3f8e35fa1009371db099198e9cf48c0296ce2452
Eliamar Aguero <eliamar.aguero@gmail.com>
2020-03-08 21:40:43 -0600

    - crear pantalla de inicio con el menú - crear pantallas vacías de ingresar y de lista (con el botón de volver) - agregar funcionalidad para viajar del home a la lista y la pantalla de ingresar

--- mensajeria/MainActivity.cs
+++ mensajeria/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Content;
 
 namespace mensajeria
 {
@@ -15,14 +16,27 @@ namespace mensajeria
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            // --------
+
+            /**
+             * Actions for the buttons on the main screen
+             */
+            Button toAddNewBtn = FindViewById<Button>(Resource.Id.toAddNewBtn);
+            Button toContactsListBtn = FindViewById<Button>(Resource.Id.toContactsListBtn);
+
+            toAddNewBtn.Click += delegate {
+                Intent createEditFormIntent = new Intent(this, typeof(CreateEditFormActivity));
+                StartActivity(createEditFormIntent);
+            };
+            toContactsListBtn.Click += delegate {
+                Intent listIntent = new Intent(this, typeof(ListActivity));
+                StartActivity(listIntent);
+            };
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            // Comentario de prueba
         }
     }
 }
