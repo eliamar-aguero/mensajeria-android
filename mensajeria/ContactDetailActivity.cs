e596e2a77480f3ae8536320905cdc54fa097cd54
Eliamar Aguero <eliamar.aguero@gmail.com>
2020-03-15 10:40:11 -0600

    - MA-19 - Agregar acciones básicas a la pantalla de detalle de contacto - agregar accion del eliminar contacto - agregar acción de redirigir a la pantalla de form al presional el botón de edit - cargar información real de la base de datos en la pantalla de detalle

--- mensajeria/ContactDetailActivity.cs
+++ mensajeria/ContactDetailActivity.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 
@@ -23,11 +24,32 @@ namespace mensajeria
             SetContentView(Resource.Layout.activity_contact_detail);
 
             /**
-             * Get data from the list
+             * Get back to the list activity
              */
-            string selectedContact = Intent.GetStringExtra("id");
-            TextView tv = FindViewById<TextView>(Resource.Id.txtName);
-            tv.Text = selectedContact;
+            Intent toContactListIntent = new Intent(this, typeof(ListActivity));
+
+            Button backToListBtn = FindViewById<Button>(Resource.Id.backToContactListBtn);
+            backToListBtn.Click += delegate {
+                StartActivity(toContactListIntent);
+            };
+
+            /**
+             * Get data from the selected contact on the activity list and pass it to the web service
+             * Get the selected contact info by name from the DB to display data in the contact details view
+             */
+            string selectedContact = Intent.GetStringExtra("name");
+            ws_mensajeria.somee.com.WebService1 ws = new ws_mensajeria.somee.com.WebService1();
+            DataSet contactInfo = new DataSet();
+
+            contactInfo = ws.GetSingleContact(selectedContact);
+            // TODO: add fallback in case of null values
+            FindViewById<TextView>(Resource.Id.txtName).Text = contactInfo.Tables[0].Rows[0]["nombre"].ToString();
+            FindViewById<TextView>(Resource.Id.txtMobileTitle).Text = contactInfo.Tables[0].Rows[0]["tel_movil"].ToString();
+            FindViewById<TextView>(Resource.Id.txtSMSTitle).Text = contactInfo.Tables[0].Rows[0]["tel_movil"].ToString();
+            FindViewById<TextView>(Resource.Id.txtPersonalPhoneTitle).Text = contactInfo.Tables[0].Rows[0]["tel_particular"].ToString();
+            FindViewById<TextView>(Resource.Id.txtWorkPhoneTitle).Text = contactInfo.Tables[0].Rows[0]["tel_trabajo"].ToString();
+            FindViewById<TextView>(Resource.Id.txtEmailTitle).Text = contactInfo.Tables[0].Rows[0]["email"].ToString();
+            FindViewById<TextView>(Resource.Id.txtIMTitle).Text = contactInfo.Tables[0].Rows[0]["direccion_im"].ToString();
 
             /**
              * Attempt to delete the contact
@@ -37,18 +59,21 @@ namespace mensajeria
                 var deleteDialog = new AlertDialog.Builder(this);
                 deleteDialog.SetMessage("Eliminar " + selectedContact + "?");
                 deleteDialog.SetNegativeButton("Cancelar", delegate { });
-                deleteDialog.SetPositiveButton("Eliminar", delegate { });
+                deleteDialog.SetPositiveButton("Eliminar", delegate {
+                    ws_mensajeria.somee.com.WebService1 ws = new ws_mensajeria.somee.com.WebService1();
+                    ws.DeleteContact(5);
+                    StartActivity(toContactListIntent);
+                });
                 deleteDialog.Show();
         	};
 
             /**
-             * Get back to the list activity
+             * Action to go to the Edit screen
              */
-            Button backToListBtn = FindViewById<Button>(Resource.Id.backToContactListBtn);
-            backToListBtn.Click += delegate {
-                Intent toContactListIntent = new Intent(this, typeof(ListActivity));
-                StartActivity(toContactListIntent);
-        	};
+            FindViewById<Button>(Resource.Id.btnEdit).Click += delegate {
+                Intent toEditIntent = new Intent(this, typeof(CreateEditFormActivity));
+                StartActivity(toEditIntent);
+            };
         }
     }
 }
